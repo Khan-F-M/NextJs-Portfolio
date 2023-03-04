ca13aa2c531840ee94fa01a051f7137cd6248536
Khan-F-M <$Faisal10>
2023-03-04 05:19:19 -0500

    First Draft of About Page Finished

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '@/styles/globals.css'
 import '@/styles/bootstrap.min.css'
 import { SSRProvider } from 'react-bootstrap';
 import Layout from '@/components/Layout';
