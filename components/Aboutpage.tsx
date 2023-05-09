105f8a4de0a883298add76e61f92c909497c360d
Khan-F-M <$Faisal10>
2023-05-09 01:09:12 -0400

    Hopeful Fix

--- components/Aboutpage.tsx
+++ components/Aboutpage.tsx
@@ -16,16 +16,15 @@ export default function Aboutpage({ abouts }: Props) {
     return (
         <>
             <div key={abouts._id} className='flex flex-col relative h-screen text-center md:text-left
-        md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center mb-10 sm:mb-5 md:mb-[-100px]'>
+        md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center mt-40 mb-40 md:mt-0 md:mb-0'>
 
-                <h3 className='md:absolute lg:absolute xl:absolute top-24 uppercase tracking-[20px]
+                    <h3 className='sm:absolute md:absolute lg:absolute xl:absolute top-24 uppercase tracking-[20px]
             text-gray-300 text-2xl'>About</h3>
 
-                <h3 className='md:absolute lg:absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>
-                    a bit about myself</h3>
+                    <h3 className='sm:absolute md:absolute lg:absolute xl:absolute top-36 uppercase tracking-[3px] 
+                text-gray-500 text-sm'>a bit about myself</h3>
 
-
-                <img src={urlFor(abouts.profilePic).url()} alt="/" className='mb-21  md:mb-0 lg:mt-10 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]' />
+                <img src={urlFor(abouts.profilePic).url()} alt="/" className='mb-9 mt-7 md:mb-0 lg:mt-10 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]' />
 
                 <div className='space-y-10 px-0 md:px-10'>
                     {/* <h4 className='text-4xl font-semibold'> Here&lsquo;s a <span className='underline decoration-[#FFF157]/50'>little</span> background</h4> */}
