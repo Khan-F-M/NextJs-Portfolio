86a2794e61ff40163eb04a19293d4ee6e5d1da1d
Khan-F-M <$Faisal10>
2023-05-09 14:01:32 -0400

    Added Footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer class="bg-black-900 rounded-lg shadow m-4 dark:bg-black-400">
             <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span class="text-sm text-white-700 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">MFaisalK™</a>
+                <span class="text-sm text-white-700 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Khan-F-M™</a>
                 </span>
                 <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
