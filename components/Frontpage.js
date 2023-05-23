5c0845eb0eb08c766b14b4c7567c6c4736503b0f
Khan-F-M <$Faisal10>
2023-05-23 02:45:44 -0400

    messed up front

--- components/Frontpage.js
+++ components/Frontpage.js
@@ -37,7 +37,7 @@ export default function Frontpage() {
                             <Cursor cursorColor="#FF0500" />
                         </h1>
                         <p className='py-4 text-white-600 max-w-[70%] m-auto'>
-                            I was a Computer Programming student at Seneca College in my final semester.
+                            I was a Computer Programming student at Seneca College.
                             I&apos;ve worked with many langauges in my time at Seneca such as C, C++, JS,
                             Python, and Linux. Along the way, I learned that I had a passion for creating
                             and designing web sites. However, due to my time focusing on college i never
