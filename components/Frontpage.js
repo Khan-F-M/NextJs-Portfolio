e3d8cb5b8347313afe0dd18818bbd2e48184b738
Khan-F-M <$Faisal10>
2025-11-18 00:21:31 -0500

    Changed my occupation

--- components/Frontpage.js
+++ components/Frontpage.js
@@ -7,7 +7,7 @@ import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 function useTypewriterHook() {
     const [text, count] = useTypewriter({
-        words: ['A Full-Stack Developer'],
+        words: ['A Software Developer'],
         loop: true,
         delaySpeed: 2000,
     });
