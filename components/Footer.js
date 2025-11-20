137d2cc463676c667ce890b707176036ea3e5b5b
Khan-F-M <$Faisal10>
2025-11-20 00:49:01 -0500

    Added the correct link in f

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
             <div className="bottom-0 left-0 w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                 <span className="text-sm text-white-700 sm:text-center dark:text-gray-400">
                     © 2023 
-                    <Link href="https://flowbite.com/"
+                    <Link href="https://github.com/Khan-F-M"
                         className="hover:underline"> Khan-F-M™</Link>
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
